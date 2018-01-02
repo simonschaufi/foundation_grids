c1858b3adab7c91efacb2e1be40882c51334cb59
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2018-01-03 00:29:42 +0100

    Set compatibility with TYPO3 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => '',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-8.99.99',
 			'gridelements' => '3.0.0-0.0.0',
 		],
 		'conflicts' => [
